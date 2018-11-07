6f925e6915297052f1784defd39fa3ad98951461
André Veríssimo <afsverissimo@gmail.com>
2018-11-07 03:12:45 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,68 @@
+const cheerio = require('cheerio');
+const request = require('request');
+const moment = require('moment');
+
+class IPMA {
+  constructor() {
+    this.caca = 1;
+  }
+
+  static get FORECAST_URL() {
+    return 'https://www.ipma.pt/pt/maritima/costeira/index.jsp';
+  }
+
+  async forecast(id) {
+    const promise = await new Promise((resolve, reject) => {
+      request
+        .get({
+          url: IPMA.FORECAST_URL,
+          qs: {
+            selLocal: id,
+            idLocal: id
+          }
+        }, (error, reponse, body) => {
+          if (error) {
+            reject(error);
+          }
+          const $ = cheerio.load(body);
+
+          const waves = {};
+
+          const today = moment().startOf('day');
+
+          $('table').each((i, el) => {
+            const day = today.format('YYYY-MM-DD');
+            waves[day] = {time: [], height: [], waves: [], power: []};
+
+            $(el).find('tr').each((j, elJ) => {
+              $(elJ).find('td').each((k, elK) => {
+                elK = $(elK);
+                switch (k) {
+                  case 0:
+                    waves[day].time.push(today.valueOf());
+                    break;
+                  case 1:
+                    waves[day].height.push(parseFloat(elK.html()));
+                    break;
+                  case 2:
+                    waves[day].waves.push(parseFloat(elK.html()));
+                    break;
+                  case 10:
+                    waves[day].power.push(parseFloat(elK.html()));
+                    break;
+                  default:
+                    break;
+                }
+              });
+              today.add(1, 'hours');
+            });
+          });
+          resolve(waves);
+        });
+    });
+
+    return promise;
+  }
+}
+
+module.exports = {IPMA};
---
+++ test/test.js
@@ -0,0 +1,15 @@
+/* eslint no-unused-vars: ["error", { "varsIgnorePattern": "[iI]gnored" }] */
+const shouldIgnored = require('should');
+const {describe, it} = require('mocha');
+const {IPMA} = require('..');
+
+const ipma = new IPMA();
+
+describe('#forecast()', () => {
+  it('responds with 3 days worth of data for \'Cabo Espichel\'', async () => {
+    const forecast = await ipma.forecast(42);
+    Object.values(forecast).should.have.length(3);
+    Object.values(forecast)[0].height.should.have.length(24);
+    Object.values(forecast)[0].time.should.have.length(24);
+  });
+});
