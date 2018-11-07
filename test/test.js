22fc56e9aa1791c4b6d34c1891beb0ac29f3ceb9
André Veríssimo <afsverissimo@gmail.com>
2018-11-07 03:15:03 +0000

    extra verbose on test name

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ const {IPMA} = require('..');
 const ipma = new IPMA();
 
 describe('#forecast()', () => {
-  it('responds with 3 days worth of data for \'Cabo Espichel\'', async () => {
+  it('responds with 3 days worth of data for \'Cabo Espichel\' (local=42)', async () => {
     const forecast = await ipma.forecast(42);
     Object.values(forecast).should.have.length(3);
     Object.values(forecast)[0].height.should.have.length(24);
