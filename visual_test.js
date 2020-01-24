9dd931b0122c942201a42a48becf8d9067ec95ce
André Veríssimo <afsverissimo@gmail.com>
2020-01-24 18:04:14 +0000

    adds visual test that shows the result

---
+++ visual_test.js
@@ -0,0 +1,16 @@
+/* eslint no-unused-vars: ["error", { "varsIgnorePattern": "[iI]gnored" }] */
+const moment = require('moment');
+const {IPMA} = require('.');
+
+const ipma = new IPMA();
+
+ipma.forecast(42).then(response => {
+  for (const el of response) {
+    console.log('date', moment(el.date).format('YYYY/MM/DD'), el.date);
+    for (const el2 of el.data) { 
+      const time = el2.time;
+      delete el2.time;
+      console.log('\t', moment(time).format('YYYY/MM/DD hh:mm'), el2);
+    }
+  }
+}).catch(error => console.log);
